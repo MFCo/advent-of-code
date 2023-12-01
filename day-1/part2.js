7026326d6e4f7de13ae762b0b50b20091e0df7c2
MFCo <marianococirio@gmail.com>
2023-12-01 14:43:32 +0100

    day 1 part 2

---
+++ day-1/part2.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const wordToNumber = (word) => {
+  const wordMap = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+
+  return word.length > 1 ? wordMap[word] : word;
+};
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+
+  const result = data.split("\n").reduce((acc, line) => {
+    const match = line.match(
+      /1|one|2|two|3|three|4|four|5|five|6|six|7|seven|8|eight|9|nine/g
+    );
+    return acc + parseInt(wordToNumber(match[0]) + wordToNumber(match[match.length - 1]));
+  }, 0);
+  console.log(result);
+});
