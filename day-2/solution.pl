b09dd03c5b12596e0c1cf3d1aa4c8d583185d7f8
MFCo <marianococirio@gmail.com>
2023-12-02 10:06:19 +0100

    part 2 day2

--- day-2/solution.pl
+++ day-2/solution.pl
@@ -4,6 +4,7 @@ use warnings;
 open(my $input, '<', 'input.txt') or die "oops";
 
 my $id_acc = 0;
+my $power_acc = 0;
 
 while (my $line = <$input>) {
     chomp($line);
@@ -19,8 +20,12 @@ while (my $line = <$input>) {
     if (!($color_max{'blue'} > 14 || $color_max{'red'} > 12 || $color_max{'green'} > 13)) {
         $id_acc += $game_id;
     }
+    
+    $power_acc += $color_max{'blue'} * $color_max{'red'} * $color_max{'green'};
 }
 
-print "Result: $id_acc\n";
+print "Result id: $id_acc\n";
+print "Result power: $power_acc\n"; 
+
 
 close($input);
